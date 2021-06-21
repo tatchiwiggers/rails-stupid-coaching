3d4dc7f41d395686ef6a7c605e9dacc4792ccc35
Tabatha Wiggers <tatisabato@hotmail.com>
2021-06-21 18:56:03 -0300

    SC done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   #verb       to: "controller/action"
   root to: "questions#home"
   get "/ask", to: "questions#ask"
-  # get "/answer", to: "questions#answer"
+  get "/answer", to: "questions#answer"
 end
